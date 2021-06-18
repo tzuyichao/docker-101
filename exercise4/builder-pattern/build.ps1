f437ce3b75452ee52a6963dad93052d1e5b05b30
TzuYi Chao <tzuyichao0313@gmail.com>
2021-06-18 14:44:34 +0800

    builder pattern

---
+++ exercise4/builder-pattern/build.ps1
@@ -0,0 +1,12 @@
+
+docker image build -t helloworld-build -f Dockerfile.build .
+
+docker container create --name helloworld-build-container helloworld-build
+
+docker container cp helloworld-build-container:/myapp/helloworld .
+
+docker image build -t helloworld .
+
+docker container rm -f helloworld-build-container
+
+rm helloworld
