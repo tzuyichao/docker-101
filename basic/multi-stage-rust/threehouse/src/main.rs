08f038783b52fe4b8c024a32801a99aafe3e9125
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-25 15:45:55 +0800

    lab

--- basic/multi-stage-rust/threehouse/src/main.rs
+++ basic/multi-stage-rust/threehouse/src/main.rs
@@ -1,5 +1,23 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_lowercase(),
+        }
+    }
+
+    fn greeting_visitor(&self) {
+        println!("{}", self.greeting)
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -13,16 +31,17 @@ fn what_is_your_name() -> String {
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut allow_them_in = false;
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true
-        }
-    }
-    if allow_them_in {
-        println!("Welcome to the TreeHousse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list")
+    let visitor_list = [
+        Visitor::new("bert", "Hello, Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi, Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+    let know_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+    
+    match know_visitor {
+        Some(visitor) => visitor.greeting_visitor(),
+        None => println!("You are not on the visitor list. Please leave.")
     }
 }
