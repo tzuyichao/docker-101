6a675be7df81793ae05c6b2a40dbd400563e7429
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-25 13:57:27 +0800

    lab

--- basic/multi-stage-rust/threehouse/src/main.rs
+++ basic/multi-stage-rust/threehouse/src/main.rs
@@ -1,10 +1,28 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    println!("Hello {}", your_name)
+    your_name
+        .trim()
+        .to_lowercase()
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
+    let visitor_list = ["bert", "steve", "fred"];
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true
+        }
+    }
+    if allow_them_in {
+        println!("Welcome to the TreeHousse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list")
+    }
 }
