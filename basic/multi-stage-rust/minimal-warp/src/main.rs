75a8508a16c1ac8b342bfa7d4e3a700e7b13b63c
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-26 20:00:26 +0800

    web development lab 1

---
+++ basic/multi-stage-rust/minimal-warp/src/main.rs
@@ -0,0 +1,11 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 8000))
+        .await;
+}
