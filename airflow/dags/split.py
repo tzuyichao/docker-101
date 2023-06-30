0f40d7b620cab8869a8dd69b8aaf15b5149b3a21
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-30 14:54:45 +0800

    lab

--- airflow/dags/split.py
+++ airflow/dags/split.py
@@ -2,45 +2,44 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
-import pyodbc
+import pymssql
 
-# 定義連接資料庫的相關資訊
-db_connection_string = "DRIVER={SQL Server Native Client 11.0};SERVER=<server_name>;DATABASE=<database_name>;UID=<username>;PWD=<password>"
+db_connection_string = {
+    'host': 'HOST',
+    'database': 'DB',
+    'user': 'ACC',
+    'password': 'PWD'
+}
 
-# 定義要執行的查詢
-query = "SELECT COUNT(*) AS total_rows FROM tableA"
+query = "SELECT COUNT(*) AS total_rows FROM dbo.HQ_MARA"
 
-# 定義每個平行任務要處理的筆數
 total_splits = 10
 
-# 定義處理每個分段的任務
 def process_data(start_row, num_rows):
-    # 建立資料庫連線
-    conn = pyodbc.connect(db_connection_string)
+    conn = pymssql.connect(**db_connection_string)
     cursor = conn.cursor()
 
-    # 執行分段查詢
-    offset_query = f"SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY <your_column>) AS RowNum, * FROM tableA) AS subquery WHERE RowNum > {start_row} AND RowNum <= {start_row + num_rows}"
-    cursor.execute(offset_query)
-    rows = cursor.fetchall()
+    print(f"start_row: {start_row}, num_rows: {num_rows}")
+    while num_rows > 0:
+        batch_size = min(2000, num_rows)
+        offset_query = f"SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY MANDT, MATNR) AS RowNum, * FROM dbo.HQ_MARA) AS subquery WHERE RowNum > {start_row} AND RowNum <= {start_row + batch_size}"
+        cursor.execute(offset_query)
+        rows = cursor.fetchall()
+        num_rows -= batch_size
+        start_row += batch_size
+        print(f"remaining rows: {num_rows}")
 
-    # 關閉資料庫連線
     cursor.close()
     conn.close()
 
-    # 將結果寫入 tableB
-    # ...
-
-# 定義 DAG
 dag = DAG(
     'split_and_process_data',
     start_date=datetime(2023, 6, 24),
     schedule_interval=None
 )
 
-# 查詢資料筆數任務
 def get_row_count():
-    conn = pyodbc.connect(db_connection_string)
+    conn = pymssql.connect(**db_connection_string)
     cursor = conn.cursor()
     cursor.execute(query)
     row_count = cursor.fetchone()[0]
@@ -48,15 +47,18 @@ def get_row_count():
     conn.close()
     return row_count
 
-# 建立查詢任務
+
 get_row_count_task = PythonOperator(
     task_id='get_row_count',
     python_callable=get_row_count,
     dag=dag
 )
 
-# 建立平行任務
+
 split_and_process_tasks = []
+dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+get_row_count_task >> dummy_task
+row_count = get_row_count()
 for i in range(total_splits):
     start_row = int(i * row_count / total_splits)
     num_rows = int(row_count / total_splits)
@@ -68,8 +70,4 @@ for i in range(total_splits):
     )
     split_and_process_tasks.append(split_and_process_task)
 
-# 建立 DummyOperator 以作為任務間的依賴關係
-dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
-
-# 設定任務間的依賴關係
 get_row_count_task >> dummy_task >> split_and_process_tasks
